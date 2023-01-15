95cfc2d0f0f8a79cd28a67ee13f749cceb095a3a
nnymhadora <janagracanin@gmail.com>
2023-01-15 21:41:02 +0100

    Tiny fixes

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,7 @@ function showDropdownMenu() {
   var x = document.getElementById("dropdown-menu");
   if (x.style.display === "flex") {
     x.style.display = "none";
-    console.log(1);
   } else {
     x.style.display = "flex";
-    console.log(2);
   }
 }
