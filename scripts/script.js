a3aeeb5155186d2e6cd189c8d276c8646988ee9e
nnymhadora <janagracanin@gmail.com>
2023-01-16 16:03:31 +0100

    Added a JS script that shows and hides a customer review on customer icon mouseover and mouseout

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,29 @@ function showDropdownMenu() {
     x.style.display = "flex";
   }
 }
+
+// The following script handles the reviews section on the indexedDB.html
+
+const customers = document.querySelectorAll(".customer-icon");
+
+customers.forEach((customer) => {
+  customer.addEventListener("mouseover", showReview);
+});
+
+customers.forEach((customer) => {
+  customer.addEventListener("mouseout", hideReview);
+});
+
+function showReview(event) {
+  var customerId = event.target.id;
+  var reviewId = customerId + "-text";
+  var review = document.getElementById(reviewId);
+  review.style.display = "flex";
+}
+
+function hideReview(event) {
+  var customerId = event.target.id;
+  var reviewId = customerId + "-text";
+  var review = document.getElementById(reviewId);
+  review.style.display = "none";
+}
